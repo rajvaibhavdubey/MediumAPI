653a3977eca8d921ef2ac627f1cbdf57b39e42f9
rajvaibhavdubey <17dec004@lnmiit.ac.in>
2018-08-07 12:21:14 +0530

    update index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var request = require("request");
+exports.handler = function(event, context, callback) {
+	callback = context.done;
+	var data = event.bodyJson || {};
+	
+
+	var username = 'username'
+	var url = "https://medium.com/@"+username+"/latest?format=json";
+	
+
+	request({
+	    url: url,
+	    json: true
+	}, function (error, response, body) {
+	    if (!error && response.statusCode === 200) {
+
+		jsonBody = JSON.parse(body.replace('])}while(1);</x>', ''));
+	        console.log(jsonBody) 
+	        callback(null, jsonBody); 
+	    }    
+	});
+};
